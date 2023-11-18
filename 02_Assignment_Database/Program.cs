9e651fe1aff2b234c424e4fb2f4c57878f57e56d
Taha the one <taha.taheri@yh.nackademin.se>
2023-11-18 15:04:07 +0100

    ny fina funktioner

--- 02_Assignment_Database/Program.cs
+++ 02_Assignment_Database/Program.cs
@@ -1,7 +1,7 @@
 ﻿using _02_Assignment_Database.ConTexts;
 using _02_Assignment_Database.Enitities;
 using _02_Assignment_Database.MenuSeervice;
-using _02_Assignment_Database.Repository;
+
 using _02_Assignment_Database.Service;
 using _02_Assignment_Database.ServiceRepository;
 using Microsoft.EntityFrameworkCore;
@@ -26,7 +26,8 @@ namespace _02_Assignment_Database
             await userMenu.BorderMainMenu();
 
             services.AddScoped<AddressEntity>();
-            services.AddScoped<CustomerRepository>();
+            services.AddScoped<CreateCustomerEntity>();
+            
 
             services.AddScoped<CustomerService>();
 
@@ -41,32 +42,3 @@ namespace _02_Assignment_Database
 
 
 
-/*
-  steg 1 
- installer nuget package 
-Microsoft.E
-ntityFrameworkCore.aqlServer 
-Microsoft.EntityFrameworkCore.Tools
-
-steg 2 
-Gör entiteter (klass/ model / objcet / tabeller 
-
-steg 3 
-skapa database och hänmta Connectionstring för att den databasen 
-Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\Taha the one\OneDrive\Skrivbord\Databas\02_Assignment_Database\02_Assignment_Database\ConTexts\Vd_Database.mdf';Integrated Security=True;Connect Timeout=30
-
-steg 4 
-skapa en Conetext som ärver ifrån Dbcontext 
-
-VD_Database            = en conetext som hanterar kopplingen till databasen.
-
- steg 5 
-Gör en add migration för att kontrallera alla (måste götas i package manager Console )
-Add-migration ang ett lämligt migrationnamn , börja med stor bokstav.
-
-
-steg 6 kontrollera så att vår Migration stämmer om den för en update-Datebase annars Remove-Migrion
-Update-Database          = spara och skapa alla tabeller i database
-
-
-  */
--- 02_Assignment_Database/Repository/TruckInfoRepository.cs
+++ 02_Assignment_Database/Repository/TruckInfoRepository.cs
@@ -2,13 +2,13 @@
 
 using _02_Assignment_Database.ConTexts;
 using _02_Assignment_Database.ServiceRepository;
+namespace _02_Assignment_Database.Repositories;
 
-namespace _02_Assignment_Database.Repository
-{
+
     internal class TruckInfoRepository : Repo<TruckInfoRepository>
     {
         public TruckInfoRepository(DataContext context) : base(context)
         {
         }
     }
-}
+
